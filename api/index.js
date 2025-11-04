5ee638d0ccd7081103f8783c09be99a464f98565
vicente0479 <yusufybalo84@gmail.com>
2025-11-04 19:27:03 -0300

    Arreglo Carpeta

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { romanToInt } = require('./romanConverter');
+const { romanToInt } = require('../romanConverter.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
